3380e821b3fb4b335f7c0660624848ac166f3108
Forinite <fortuneobe2@gmail.com>
2026-09-16 14:43:24 +0100

    Added Profile and Audio view

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -599,7 +599,18 @@ function getAlarmState(dateTime: string) {
       )}
     </div>
 
-    {/* <div className="dashboard-sidebar">
+        {/* <audio
+          style={{
+            visibility: 'hidden',
+          }}
+          className="audio-player"
+          ref={Audio}
+          src="/audio.mp3"
+          controls
+          loop
+        /> */}
+
+    <div className="dashboard-sidebar">
       <section className="author-card">
         <div className="author-icon">◯</div>
 
@@ -638,7 +649,7 @@ function getAlarmState(dateTime: string) {
           Play Audio
         </button>
       </section>
-    </div> */}
+    </div>
   </main>
 </div>
 
