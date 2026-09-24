8b17ffa2f21eb9b9ed634d9349a2d0fec5caa62a
Forinite <fortuneobe2@gmail.com>
2026-09-24 21:49:45 +0100

    []

--- public/push.js
+++ public/push.js
@@ -2,7 +2,7 @@ var push = require('web push')
 
 let vapidKeys = {
     publicKeys: "BHOKUpO2splGJyFpzURcNyiGbzth4eV4YClxwRv3MYy9JgLoXbFhIoR46jKiFyxXSiRYft9sj2mAxO3BgQdC8Jg",
-    privateKeys: "ogAWmk5WEK0kqL4-bxpu1LL6kai-9Gqn8yMBywc-aKo"
+    privateKeys: meta.env.VAPID_PRIVATE_KEY
 }
 
 push.setVapidDetails('mailto:random@mail.com', vapidKeys.publicKeys, vapidKeys.privateKeys)
